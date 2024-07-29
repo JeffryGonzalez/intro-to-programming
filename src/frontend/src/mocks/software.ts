fddd46418210efafde927393300a8a0b468177df
Jeffry Gonzalez <jeff@hypertheory.com>
2024-07-29 15:21:40 -0400

    before break

--- src/frontend/src/mocks/software.ts
+++ src/frontend/src/mocks/software.ts
@@ -3,7 +3,8 @@ import { http, HttpResponse } from "msw";
 const response = [
     { id: "1", title: "Destiny 2"},
     { id: "2", title: "Jetbrains Rider"},
-    { id: "3", title: "Notepad"}
+    { id: "3", title: "Notepad"},
+    { id: "4", title: "Some Software with an incredibly long name"}
 ]
 const handlers = [
     http.get("http://localhost:1337/api/software", () => {
