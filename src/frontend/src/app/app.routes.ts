51e968dcb4c39b9ce165951234ffe6c94d5acbfa
Jeffry Gonzalez <jeff@hypertheory.com>
2024-07-29 14:49:40 -0400

    have our fake books. let's get busy

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { IssueListComponent } from './components/issue-list.component';
+import { NewIssueComponent } from './components/new-issue.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'new-issue',
+        component: NewIssueComponent
+    },
+    {
+        path: 'issues',
+        component: IssueListComponent
+    }
+];
