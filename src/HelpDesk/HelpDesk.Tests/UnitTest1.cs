2e6288ebbff6404b57f9c823a30ff1fd7866bb85
Jeffry Gonzalez <jeff@hypertheory.com>
2024-07-29 16:26:25 -0400

    before api test

--- src/HelpDesk/HelpDesk.Tests/UnitTest1.cs
+++ src/HelpDesk/HelpDesk.Tests/UnitTest1.cs
@@ -2,11 +2,26 @@ namespace HelpDesk.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Fact] // attribute @test
+    public void TenPlusTwentyIsThirty()
     {
-        // When we do a GET request to /api/software, we get:
-        // -- a 200 Ok Status Code
-        // -- We get a list of software.
+        // Given
+        int a = 10; int b = 20;
+        int answer;
+        // When
+        answer = a + b;
+        // Then
+        Assert.Equal(30, answer);
+    }
+
+    [Theory]
+    [InlineData(10, 20, 30)]
+    [InlineData(2, 2, 4)]
+    [InlineData(10, 2, 12)]
+    public void CanAddTwoIntegers(int a, int b, int expected)
+    {
+        int answer = a + b;
+
+        Assert.Equal(expected, answer);
     }
 }
