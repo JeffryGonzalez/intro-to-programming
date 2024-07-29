2d4bb149a3518e6cf952ff96316f95c1ce976938
Jeffry Gonzalez <jeff@hypertheory.com>
2024-07-29 16:50:55 -0400

    done for day 1

---
+++ src/HelpDesk/HelpDesk.Tests/Software/GettingSoftware.cs
@@ -0,0 +1,22 @@
+﻿
+using Alba;
+
+namespace HelpDesk.Tests.Software;
+public class GettingSoftware
+{
+    // Test that we can make an HTTP GET requests to /api/software
+
+    [Fact]
+    public async Task CanGetSoftware()
+    {
+        var host = await AlbaHost.For<Program>();
+
+        await host.Scenario(api =>
+        {
+            api.Get.Url("/api/software");
+            api.StatusCodeShouldBeOk();
+        });
+
+
+    }
+}
