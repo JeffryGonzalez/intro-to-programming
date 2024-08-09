d79b60c5a21150b3c73b032a425d1596ff17e73a
Jeffry Gonzalez <jeff@hypertheory.com>
2024-08-09 16:07:26 -0400

    :shipit:

---
+++ demos/docker1/src/hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello from a C Application running in unbuntu! v0.2\n");
+    return (0);
+}
---
+++ notes/friday-aug-9.md
@@ -0,0 +1,11 @@
+# Good Morning! ☕🌅
+
+It's FRIDAY, August 9, 2024.
+
+## Today
+
+ALL THE REST OF EVERYTHING
+
+Containers
+
+Deployment (CD)
